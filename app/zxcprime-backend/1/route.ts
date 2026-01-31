57350c0feb4da9cacf4e1304480240da9797ce90
iamDaedalus <jerometecson.basc@gmail.com>
2026-01-31 18:17:16 +0800

    fgfgf

--- app/zxcprime-backend/1/route.ts
+++ app/zxcprime-backend/1/route.ts
@@ -64,7 +64,7 @@ export async function GET(req: NextRequest) {
         headers: {
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-          Referer: "https://videasy.net/",
+          Referer: "https://player.videasy.net/",
         },
       },
       5000,
