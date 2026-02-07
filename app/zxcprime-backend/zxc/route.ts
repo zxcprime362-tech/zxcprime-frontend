dceab7ae29c0c2c5c7f93fbd2db140554d835e48
jerometecson-main <jerometecson.main@gmail.com>
2026-02-07 21:59:31 +0800

    added new referrer

--- app/zxcprime-backend/zxc/route.ts
+++ app/zxcprime-backend/zxc/route.ts
@@ -33,6 +33,7 @@ export async function GET(req: NextRequest) {
     if (
       !referer.includes("/api/") &&
       !referer.includes("localhost") &&
+      !referer.includes("zxcprime-icu.netlify.app") &&
       !referer.includes("http://192.168.1.4:3000/") &&
       !referer.includes("https://www.zxcprime.icu/") &&
       !referer.includes("https://zxcprime.icu/") &&
