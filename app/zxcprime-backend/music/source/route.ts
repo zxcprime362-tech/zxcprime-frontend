97be51e9338cc303ea0612b13a2b052dd37bee25
jerometecson-main <jerometecson.main@gmail.com>
2026-02-10 10:35:13 +0800

    .

--- app/zxcprime-backend/music/source/route.ts
+++ app/zxcprime-backend/music/source/route.ts
@@ -14,6 +14,7 @@ export async function GET(req: NextRequest) {
         "User-Agent":
           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         Accept: "application/json, text/plain, */*",
+        Referer: "https://tidal-api.binimum.org/",
       },
     });
 
