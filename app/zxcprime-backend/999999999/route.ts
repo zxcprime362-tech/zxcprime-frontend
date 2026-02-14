8e4abaeb3e272149a6f3d097d5fed3b49b9b8e0e
jerometecson-main <jerometecson.main@gmail.com>
2026-02-15 01:52:16 +0800

    update new workers

--- app/zxcprime-backend/999999999/route.ts
+++ app/zxcprime-backend/999999999/route.ts
@@ -159,6 +159,7 @@ export async function GET(req: NextRequest) {
     const videoUrl = sortedDownloads[0].url;
 
     const proxies = [
+      "https://late-snowflake-5076.zxcprime362.workers.dev/",
       "https://orange-paper-a80d.j61202287.workers.dev/",
       "https://weathered-frost-60b0.zxcprime361.workers.dev/",
       "https://long-frog-ec4e.coupdegrace21799.workers.dev/",
